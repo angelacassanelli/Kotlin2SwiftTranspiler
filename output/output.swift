3724fab520be1d638a506b67cc185158d2757bf7
angelacassanelli <angelacassanelli98@gmail.com>
2024-12-05 15:56:05 +0100

    test

--- output/output.swift
+++ output/output.swift
@@ -1,3 +1,5 @@
 class Example() {
-func test(a: Bool) -> Bool { return !a }
+func exampleFun() { var x : Int = 10
+let y : String = "Hello"
+if x > 5 { print("x is greater than 5") } else { print("x is less than or equal to 5") } }
 }
