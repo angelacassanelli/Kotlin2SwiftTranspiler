ab0c89f5c791b61cf6afa752ff5e1f7c30385f1b
angelacassanelli <angelacassanelli98@gmail.com>
2024-12-07 11:53:35 +0100

    test

--- output/output.swift
+++ output/output.swift
@@ -1,6 +1,3 @@
-class Math() {
-func add(a: Int, b: Int) -> Int { return a + b }
-func main() { var sum = add(a: 5, b: 7)
-# Error: Assignment before declaration
-print(sum) }
+class Example() {
+var a : String
 }
