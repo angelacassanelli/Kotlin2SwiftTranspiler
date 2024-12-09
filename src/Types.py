0a3055b59c09db94c2141a1c63aa4796cecefac5
angelacassanelli <angelacassanelli98@gmail.com>
2024-12-09 18:27:47 +0100

    adding doc

--- src/Types.py
+++ src/Types.py
@@ -1,9 +1,30 @@
 from enum import Enum
 
+"""
+KotlinTypes(Enum):
+    An enumeration representing Kotlin data types.
+
+    Members:
+        INT (str): Represents the Kotlin 'Int' type.
+        STRING (str): Represents the Kotlin 'String' type.
+        BOOLEAN (str): Represents the Kotlin 'Boolean' type.
+"""
+
 class KotlinTypes(Enum):
     INT = "Int"
     STRING = "String"
     BOOLEAN = "Boolean"
+
+
+"""
+SwiftTypes(Enum):
+    An enumeration representing Swift data types.
+
+    Members:
+        INT (str): Represents the Swift 'Int' type.
+        STRING (str): Represents the Swift 'String' type.
+        BOOLEAN (str): Represents the Swift 'Bool' type.
+"""
 
 class SwiftTypes(Enum):
     INT = "Int"
--- src/Utils.py
+++ src/Utils.py
@@ -1,10 +1,33 @@
 from Types import *
 
+"""
+KOTLIN_2_SWIFT_TYPES:
+    A dictionary mapping Kotlin data types to their equivalent Swift types.
+
+    Keys:
+        KotlinTypes.INT: Corresponds to the Swift type SwiftTypes.INT.
+        KotlinTypes.STRING: Corresponds to the Swift type SwiftTypes.STRING.
+        KotlinTypes.BOOLEAN: Corresponds to the Swift type SwiftTypes.BOOLEAN.
+
+"""
+
 KOTLIN_2_SWIFT_TYPES = {
     KotlinTypes.INT: SwiftTypes.INT,
     KotlinTypes.STRING: SwiftTypes.STRING,
     KotlinTypes.BOOLEAN: SwiftTypes.BOOLEAN
 }
+
+
+"""
+RESERVED_KEYWORDS:
+    A set of keywords and symbols reserved in Kotlin and Swift. These keywords are not allowed 
+    to be used as variable or function names in either language.
+
+    Contains:
+        Common keywords like "if", "else", "for", "class", "fun", "return", etc.
+        Operators such as "+", "-", "*", "/", "%", and comparison operators like "==", ">", "<", etc.
+        Brackets and punctuation like ",", ";", ":", ".", "(", ")", "{", "}", "[", "]", etc.
+"""
 
 RESERVED_KEYWORDS = {
     "readLine", "println", "val", "var", "Boolean", "Int", "String", 
@@ -13,3 +36,5 @@ RESERVED_KEYWORDS = {
     ",", ";", ":", ".", "(", ")", "{", "}", "[", "]", "&&", "||",
     "!", "..", "\"", "\'"
 }
+
+
